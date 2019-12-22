6518f136586994d851a13a9c20579e15ec6fdb98
Hyeonmin <kahelum@gmail.com>
2019-12-22 21:45:07 +0900

    commit

--- Rhythm Game/src/rhythm_game_1/RhythmGame.java
+++ Rhythm Game/src/rhythm_game_1/RhythmGame.java
@@ -1,8 +1,17 @@
 package rhythm_game_1;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class RhythmGame extends JFrame {
+	
+	private Image screenImage;
+	private Graphics screenGraphics;
+	
+	private Image introBackground;
 	
 	public RhythmGame() {
 		setTitle("Rhythm Game");
@@ -10,7 +19,22 @@ public class RhythmGame extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);		
+		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+		
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphics = screenImage.getGraphics();
+		screenDraw(screenGraphics);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
 	}
 	
 }
